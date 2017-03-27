98dcb4a43ed2874f75116a78922329c980342f57
merry75 <benjiab@hotmail.com>
2017-03-27 16:26:09 +0300

    beer server authentification

--- server.js
+++ server.js
@@ -4,122 +4,41 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/beers');
 
-var Beer = require("./BeerModel");
+var beerRoutes = require('./routes/beerRoutes');
+var userRoutes = require('./routes/userRoutes');
+var User = require("./models/UserModel");
 
-var bodyParser = require('body-parser')
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var LocalStrategy = require('passport-local').Strategy;
+var expressSession = require('express-session');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-// app.get('/', function(req, res) {
-//   res.send('Testing Server')
-// })
 
-app.get('/beers', function (req, res, next) {
-  Beer.find(function (error, beers) {
-    if (error) {
-      //console.log(error)
-      console.error(error)
-      return next(error);
-    } else {
-      console.log(beers)
-      res.json(beers);
-    }
-  });
-});
+// Configure passport and session middleware
+app.use(expressSession({
+  secret: 'yourSecretHere',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/beers/:id', function(req, res, next) {
-  Beer.findById(req.params.id, function(error, beer) {
-    if (error) {
-      console.error(error)
-      return next(error);
-    } else {
-      res.send(beer);
-    }
-  });
-});
+// Configure passport-local to use user model for authentication
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.post('/beers', function(req, res, next) {
-  var beer = new Beer(req.body);
-
-  beer.save(function(err, beers) {
-    if (err) {
-      console.error(err)
-      return next(err);
-    } else {
-      res.json(beers);
-    }
-  });
-});
-
-app.post('/beers/:id/reviews', function(req, res, next) {
-  Beer.findById(req.params.id, function(err, foundBeer) {
-    if (err) {
-      console.error(err);
-      return next(err);
-    } else if (!foundBeer) {
-      return res.send("Error! No beer found with that ID");
-    } else {
-      foundBeer.reviews.push(req.body)
-      foundBeer.save(function(err, updatedBeer) {
-        if (err) {
-          return next(err);
-        } else {
-          res.send(updatedBeer);
-        }
-      });
-    }
-  });
-});
-
-app.delete('/beers/:beerId/reviews/:reviewId', function (req,res,next) {
-    Beer.findById(req.params.beerId, function(err, foundBeer) {
-    if (err) {
-      return next(err);
-    } else if (!foundBeer) {
-      return res.send("Error! No beer found with that ID");
-    } else {
-      var reviewToDelete = foundBeer.reviews.id(req.params.reviewId)
-      if (reviewToDelete) {
-        reviewToDelete.remove()
-        foundBeer.save(function(err, updatedBeer) {
-          if (err) {
-            return next(err);
-          } else {
-            res.send(updatedBeer);
-          }
-        });
-      } else {
-        return res.send("Error! No review found with that ID");
-      }
-    }
-  });
-});
-
-app.delete('/beers/:id', function(req, res, next) {
-  console.log("hadas"+req.params.id);
-  Beer.remove({ _id: req.params.id }, function(err, beers) {
-    if (err) {
-      console.error(err)
-      // return next(err);
-    } else {
-      res.json(beers);
-    }
-  });
-});
-
-app.put('/beers/:id', function(req, res, next) {
-  Beer.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true}, function(err, beer) {
-    if (err) {
-      console.error(err)
-      return next(err);
-    } else {
-      res.send(beer);
-    }
-  });
-});
+//This tells the server that when a request comes into '/beers'
+//that it should use the routes in 'beerRoutes'
+//and those are in our new beerRoutes.js file
+app.use('/beers', beerRoutes);
+app.use('/users', userRoutes);
 
 // error handler to catch 404 and forward to main error handler
 app.use(function(req, res, next) {
@@ -137,6 +56,7 @@ app.use(function(err, req, res, next) {
     error: err
   });
 });
+
 
 
 app.listen('8000', function() {
